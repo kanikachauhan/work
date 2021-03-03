ef5ef4953b8563de96929e9dda7a388086d917b7
kanikachauhan <kanika.chauhan@ymail.com>
2021-03-03 19:27:13 +0530

    Add files via upload

---
+++ myshell.c
@@ -0,0 +1,32 @@
+Script started on 2021-03-03 18:39:33+05:30 [TERM="xterm-256color" TTY="/dev/pts/0" COLUMNS="79" LINES="29"]
+]0;user@user-VirtualBox: ~/Desktop[01;32muser@user-VirtualBox[00m:[01;34m~/Desktop[00m$ nano -[K[1Pexit[2Pls./myshell [15@gcc myshell.c -o [C[C[C[C[C[C[C[C
+]0;user@user-VirtualBox: ~/Desktop[01;32muser@user-VirtualBox[00m:[01;34m~/Desktop[00m$ gcc myshell.c -o myshell exit[K[2Pls[K[K./myshell 
+COMMAND->
+ cp m	yshell tempfile
+COMMAND->
+ ls -l etre    tempfile
+-rwxrwxr-x 1 user user 17648 Mar  3 18:40 tempfile
+COMMAND->
+ time sk leep 5
+0.00user 0.00system 0:05.00elapsed 0%CPU (0avgtext+0avgdata 1996maxresident)k
+0inputs+0outputs (0major+75minor)pagefaults 0swaps
+COMMAND->
+ time sleep 15 &
+sleep: invalid time interval ‘&’
+Try 'sleep --help' for more information.
+Command exited with non-zero status 1
+0.00user 0.00system 0:00.00elapsed 100%CPU (0avgtext+0avgdata 2096maxresident)k
+0inputs+0outputs (0major+80minor)pagefaults 0swaps
+COMMAND->
+ date
+Wednesday 03 March 2021 06:40:46 PM IST
+COMMAND->
+ r
+COMMAND->
+ r 2
+COMMAND->
+ exit
+]0;user@user-VirtualBox: ~/Desktop[01;32muser@user-VirtualBox[00m:[01;34m~/Desktop[00m$ exit
+exit
+
+Script done on 2021-03-03 18:41:18+05:30 [COMMAND_EXIT_CODE="0"]
